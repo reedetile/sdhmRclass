25127841b0e70a217d8b3788cd5ff9fc963160ba
lindseypekurny <71886366+lindseypekurny@users.noreply.github.com>
2022-02-24 15:30:54 -0500

    Create sdhmR_EX06_PekurnyLindsey.R
    Exercise 6

---
+++ ex6/sdhmR_EX06_PekurnyLindsey.R
@@ -0,0 +1,140 @@
+#sdhmR Exercise 06
+#02/23/2022
+#RCS & LAP
+
+#----------Global options---------#
+#Setting Paths
+path.root <- "C:/Users/14842/Documents/SDHM/sdhmR-V2022.1"
+path.ex <- paste(path.root, "/data/exercise/traindat", sep = "") #Path to mod 2
+path.preds <- paste(path.root, '/data/exercise/preds', sep = '')
+path.figs <- paste(path.root, "/powerpoints/figures", sep = "") #path to save figs
+path.gis <- paste(path.root, "/data/gis_layers", sep = "") #path to gis files
+
+# some libraries
+library(raster)	# FXNS: extent, raster, res, rasterize, values, writeRaster, 
+#       cellFromXY, extract
+library(sf)    	# FXNS: st_as_sf, st_as_sfc, st_bbox, st_convex_hull, st_union
+#       st_buffer, st_read, st_write, st_intersection, st_geometry,
+#       st_drop_geometry, st_coordinates, st_sf
+library(terra)	# FXNS: as.data.frame, rast, vect
+library(sp)
+library(dplyr)
+
+# source course CRSs
+setwd(path.root)
+source("r_code/prjs4_sdhmR.r")# source course CRSs
+#source("C:/Users/14842/Documents/SDHM/sdhmR-V2022.1/r_code/prjs4_sdhmR.r")
+ls(pattern = "prj.") # should be 5 prj.* objects
+
+######Question 1######
+######################
+# Break the predictor variables into logical groups of **topography**, **temperature**, and **precipitation**.  Do this by creating a new column called **`pred_type`** and add it the dataframe.
+
+table(pers.trTOPO['PERS106'])
+  
+unique(pers.trTOPO$PERS106)
+unique(pers.trTOPO$etpt_5)
+
+## Question #2
+#* Assess correlations:
+#  * Among all variables; and
+#* Within each of the logical groupings created above
+
+# numeric correlations
+cut.point <- 0.7 # set cutpoint for correlation
+c1 <- cor(pers.trTOPO[, c(27:42)],
+          use = "pairwise.complete.obs", 
+          method = "spearman") # est. correlation
+c1 # examine
+c2 <- subset(c1 > cut.point | c1 < -cut.point) # matrix of cor>cutpoint
+c2 # examine; FALSE indicates cor<cutpoint
+
+panel.cor <- function(x, y,
+                      digits=2,
+                      prefix="",
+                      cex.cor) 
+{ usr <- par("usr"); on.exit(par(usr)) 
+par(usr = c(0, 1, 0, 1)) 
+r <- abs(cor(x,
+             y,
+             use = "pairwise.complete.obs")) 
+txt <- format(c(r,
+                0.123456789),
+              digits = digits)[1] 
+txt <- paste(prefix,
+             txt,
+             sep = "") 
+if(missing(cex.cor)) cex <- 0.8/strwidth(txt) 
+
+test <- cor.test(x,y) 
+# borrowed from printCoefmat
+Signif <- symnum(test$p.value,
+                 corr = FALSE,
+                 na = FALSE, 
+                 cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                 symbols = c("***", "**", "*", ".", " ")) 
+
+text(0.5,
+     0.5,
+     txt,
+     cex = cex * r)
+text(.8,
+     .8,
+     Signif,
+     cex=cex,
+     col=2) 
+}
+
+pairs(pers.trTOPO[,
+              c(27:42)],
+      lower.panel = panel.smooth, 
+      upper.panel = panel.cor,
+      main = "Topo Variables") 
+
+## Question #3
+
+#* Examine variable importance of each predictor variable as related to presence:absence using the process described in the Module.     
+
+#### START function variable importance
+varimp.glm <- function(tr.spp,
+                       tr.var,
+                       pres,
+                       pf,
+                       pl) {
+  tmp.mat <- matrix(ncol = 2,
+                    nrow = (pl - pf + 1))
+  for (i in pf:pl) {
+    # option: linear+quadratic; linear only
+    tmp <- glm(tr.spp[, pres] ~ tr.var[, i] + 
+                 I((tr.var[, i])^2),
+               na.action = na.omit, 
+               family = binomial)
+    # linear only glm
+    #tmp <- glm(tr.spp[, pres] ~ tr.var[, i], na.action = na.omit, family = binomial)
+    tmp.mat[(i - pf + 1), 1] <- tmp$aic
+    tmp.mat[(i - pf + 1), 2] <- (1 - (tmp$deviance/tmp$null.deviance))
+  }
+  return(tmp.mat)
+} 
+#### END function variable importance
+
+
+
+
+## Question #4
+
+#* Eliminate redundant variables with a goal of retaining 7-10 of the 16 available
+#* Justify your decision(s) to keep / remove variables using bullets
+
+#---
+  
+  ## Question #5
+  
+#  * Include the term **tr_MORT**, **tr_PERS**, **tr_RANG**, and **tr_SEED** somewhere in the final training data objects, depending on your data group.  Use of **tr_ByGroup** here means the data are now ready for model creation.
+#* Save your data as R objects:
+#  * Dataframe;
+#* Point shapefile with geometry in R
+#* Export as a point shapefile in ESRI format
+#* Save these R objects in a **`.RData`** file
+
+#These data will be used as we next begin the SDHM model constructions.
