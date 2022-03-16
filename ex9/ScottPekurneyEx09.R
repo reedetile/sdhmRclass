9133efac31e836ad29ab58defd8d1840da2f8176
Reed Scott <rscott1294@gmail.com>
2022-03-16 12:51:55 -0400

    yo lindsey wtf

---
+++ ex9/ScottPekurneyEx09.R
@@ -0,0 +1,213 @@
+#sdhm ex9
+#RCS & LAP
+#3/14/2022
+
+#########################################################
+#Global options
+#Create paths
+#load libraries
+#load map projections
+
+path.root <- "D:/OneDrive - University of Vermont/Classes/Spring2022/sdhmR/sdhmR-V2022.1" #Reed Laptop Path
+#path.root <- "C:/Users/14842/Documents/SDHM/sdhmR-V2022.1" #Lindsey Path
+path.ex <- paste(path.root, "/data/exercise/traindat", sep = "") #Path to mod 2
+path.preds <- paste(path.root, '/data/exercise/preds', sep = '')
+path.figs <- paste(path.root, "/powerpoints/figures", sep = "") #path to save figs
+path.gis <- paste(path.root, "/data/gis_layers", sep = "") #path to gis files
+
+# some libraries
+library(raster)	# FXNS: extent, raster, res, rasterize, values, writeRaster, 
+#       cellFromXY, extract
+library(sf)    	# FXNS: st_as_sf, st_as_sfc, st_bbox, st_convex_hull, st_union
+#       st_buffer, st_read, st_write, st_intersection, st_geometry,
+#       st_drop_geometry, st_coordinates, st_sf
+library(terra)	# FXNS: as.data.frame, rast, vect
+library(sp)
+library(dplyr)
+library(tidyverse)
+library(data.table)
+library(modeest)
+library(DAAG)
+library(PresenceAbsence)
+options("rgdal_show_exportToProj4_warnings"="none") # run this before library(rgdal)
+library(rgdal)
+library(dismo)
+library(maptools)
+
+# source course CRSs
+setwd(path.root)
+getwd()
+source("r_code/prjs4_sdhmR.r")
+ls(pattern = "prj.") # should be 5 prj.* objects
+
+###########################################################
+#Import and explore data
+#NOTE:  intent is not to reduce number of variables; that was completed in exercise #5.  Rather, calculate simple descriptive statistics (mean, sd, n) and boxplots
+
+setwd(path.ex)
+load('tr_PERS.RData')
+class(tr_PERS)
+head(tr_PERS)
+drops <- c('wgs_xF','wgs_yF','cell.wgs_x','cell.wgs_y','exp5nrm','exp3nrm','rough_1k','prad_sw_di',
+           'etpt_5','mind_yr_av','prec_winte','tave_sprin','UNIQUEID.x'
+           ,'UNIQUEID.y')
+tr_PERS <- tr_PERS[ ,!(names(tr_PERS) %in% drops)]
+tr_PERS <- na.omit(tr_PERS)
+dim(tr_PERS)
+
+
+#explore etpt5
+mean(tr_PERS$etpt_5.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$etpt_5.img)) #checked for number of NAs
+sd(tr_PERS$etpt_5.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$etpt_5.img)) #check total N
+
+#explore etpt6
+mean(tr_PERS$etpt_6.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$etpt_6.img)) #checked for number of NAs
+sd(tr_PERS$etpt_6.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$etpt_6.img)) #check total N
+#explore etpt_sprin
+mean(tr_PERS$etpt_sprin.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$etpt_sprin.img)) #checked for number of NAs
+sd(tr_PERS$etpt_sprin.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$etpt_sprin.img)) #check total N
+#explore mind_yr_ave
+mean(tr_PERS$mind_yr_av.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$mind_yr_av.img)) #checked for number of NAs
+sd(tr_PERS$mind_yr_av.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$mind_yr_av.img)) #check total N
+#explore prec_winte
+mean(tr_PERS$prec_winte.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$prec_winte.img)) #checked for number of NAs
+sd(tr_PERS$prec_winte.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$prec_winte.img)) #check total N
+#explore tave_s_hal
+mean(tr_PERS$tave_s_hal.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$tave_s_hal.img)) #checked for number of NAs
+sd(tr_PERS$tave_s_hal.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$tave_s_hal.img)) #check total N
+#explore tave_sprin
+mean(tr_PERS$tave_sprin.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$tave_sprin.img)) #checked for number of NAs
+sd(tr_PERS$tave_sprin.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$tave_sprin.img)) #check total N
+#explore tmax_summe
+mean(tr_PERS$tmax_summe.img, na.rm = T) #mean returned NA
+sum(is.na(tr_PERS$tmax_summe.img)) #checked for number of NAs
+sd(tr_PERS$tmax_summe.img, na.rm = T) #standard deviation
+nrow(tr_PERS) - sum(is.na(tr_PERS$tmax_summe.img)) #check total N
+
+
+#Boxplots
+par(mfrow = c(3, 3))
+boxplot(tr_PERS$etpt_5.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "etpt5", na.rm = T)
+boxplot(tr_PERS$etpt_6.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "etpt6", na.rm = T)
+boxplot(tr_PERS$etpt_sprin.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "etpt_sprin", na.rm = T)
+boxplot(tr_PERS$mind_yr_av.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "mind_yr_ave", na.rm = T)
+boxplot(tr_PERS$prec_winte.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "prec_winte", na.rm = T)
+boxplot(tr_PERS$tave_s_hal.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "tave_s_hal", na.rm = T)
+boxplot(tr_PERS$tave_sprin.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "tave_sprin", na.rm = T)
+boxplot(tr_PERS$tmax_s_hal.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "tmax_s_hal", na.rm = T)
+boxplot(tr_PERS$tmax_summe.img ~ tr_PERS$PERS106, xlab = "Presence:Absence", 
+        ylab = "tmax_summe", na.rm = T)
+
+#################################################################################
+#Question 2
+# * Construct a MAXENT model using the presence:absence data
+# * Build plots of variable importance values
+
+head(tr_PERS,2)
+PERS.xy <- tr_PERS[,3:4]
+#Building raster stack + domain
+setwd(path.preds)
+pers.list <- list.files(pattern = ".img$") # list of .img files; $ strips extra
+pers.list # examine
+pers.dom <- stack(pers.list) # build raster stack
+pers.dom # examine stack
+names(pers.dom) # examine stack names
+
+# save 20% presence for testing, 80% training; 5-fold x-val
+set.seed(1234) # set.seed if desire repeatability
+#library(dismo) # dismo needed for kfold and maxent
+fold <- kfold(PERS.xy, k = 5) # data k-folds == 5
+pers.tst <- PERS.xy[fold == 1, ] # build test data; the 20%
+pers.tr <- PERS.xy[fold != 1, ] # build training data; the 80%
+
+# run maxent model; requires dismo
+mod1.MAX <- maxent(pers.dom, pers.tr) # basic maxent w/training x,y data 
+mod1.MAX
+plot(mod1.MAX)
+
+#################################################################################
+#Question 3
+# * Calculate accuracy metrics (as in Module 3.2 and 3.3, Analytical Intermission) using:
+# * Resubstitution approaches, and
+# * A 10-fold cross--validation approach
+
+# Simple 10 fold code
+set.seed(1234) # set.seed for repeatability
+pers.xf <- sample(rep(c(1:10), length = nrow(tr_PERS))) # vector of random xfolds
+mod1.predXF <- rep(0, length = nrow(tr_PERS)) # empty vector of 0
+xfold <- 10 # set No. xfolds
+# simple loop for xfold
+for (i in 1:xfold) {
+        tr <- tr_PERS[pers.xf != i, ] # training not eq. i
+        te <- tr_PERS[pers.xf == i, ] # test eq. i
+        mx <- maxent(tr_PERS[5:ncol(tr_PERS)], tr_PERS[2], data = tr) # maxent model on training. first value = predictors, second = pres/abs
+        mod1.predXF[pers.xf == i] <- predict(mx, te) # predict to test
+}
+head(mod1.predXF) # examine vector xfold prediction 
+
+# generate confusion matrix; see help(theshold) for maxent threshold options
+# evaluate model (an x-fold process: see help(evaluate))
+mod1.val <- evaluate(mod1.MAX, p = tr_PERS[tr_PERS$PERS106 == 1, c(3:4)], 
+                     a = tr_PERS[tr_PERS$PERS106 == 0, c(3:4)], x = pers.dom) # x-fold cross-val
+mod1.val # examine
+threshold(mod1.val) # view maxent thresholds 
+#   here use spec_sens: highest sum of sens & spec
+modl <- 'mod1.MAX'
+pers.max.XF <- cbind(modl, tr_PERS[2], mod1.predXF) # build dataframe w/mod1 predictions
+head(pers.max.XF, 2) # examine prediction dataframe
+mod.cutXF <- threshold(mod1.val)
+mod.cutXF # view maxent thresholds
+mod1.cfmatXF <- table(pers.max.XF[[2]], factor(as.numeric(pers.max.XF$mod1.predXF >= mod.cutXF$equal_sens_spec)))
+mod1.cfmatXF
+########################################################################################################
+
+pers <- 'PERS106'
+#Resubstitution
+mod1.MAX.pred <- predict(pers.dom, mod1.MAX, type = 'prob')
+head(mod1.MAX.pred)
+
+#10x cross-val
+set.seed(1234) # set and save seed if desire replicability of samples
+glm.cv10 <- CVbinary(mod1.MAX, nfolds = 10, print.details = F) # cross validate model w/10 folds
+ls(glm.cv10) # examine crossval object
+head(glm.cv10$cvhat) # examine 
+
+
+
+# examine resubstitution and cross-validation estimates
+head(mod1.MAX.pred) # examine resub prediction
+head(glm.cv10$cvhat) # examine xval prediction
+glm.cvpred <- glm.cv10$cvhat # assign new name to crossval estimates
+cross.val <- cbind(pers, tr_PERS[2], mod1.MAX.pred, glm.cvpred) # build dataframe
+head(cross.val, 2) # examine; NOTE will differ each run unless seed is saved 
+
+#set threshold using PresenceAbsence package + calculate model accuratcies
+mod.cut <- optimal.thresholds(cross.val, opt.methods = c("ObsPrev")) # threshold=PREVALENCE
+mod.cut # examine threshold=PREVALENCE
+
+mod1.acc <- presence.absence.accuracy(cross.val, threshold = mod.cut$mod1.MAX.pred)
+tss <- mod1.acc$sensitivity + mod1.acc$specificity - 1 # code TSS metric
+mod1.acc <- cbind(mod1.acc[1:7], tss) # bind all metrics
+head(mod1.acc) # examine
